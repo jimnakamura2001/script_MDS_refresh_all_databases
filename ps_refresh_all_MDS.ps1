2537eeb3833deb826d8388a4a8972b34baa7a536
Confiabilidade Manut (SHE&S) <confiabilidade.manut@cabotcorp.com>
2025-06-11 12:49:15 -0300

    Add initial PowerShell script and requirements for MDS refresh process

---
+++ ps_refresh_all_MDS.ps1
@@ -0,0 +1,7 @@
+$python = "C:\Users\CManut\AppData\Local\Programs\Python\Python313\python.exe"
+
+$script = "C:\Users\CManut\Documents\scripts\script_MDS_refresh_all_databases\refresh_all_MDS.py"
+
+& $python $script
+
+exit
