4d02fb371163e8d9313c090826f9c4fa4dcaa277
Jim Nakamura <jim.nakamura@cabotcorp.com>
2025-07-03 11:53:16 -0300

    fix: Update sensitive configuration data in encrypt_forecast.py

--- encrypt.py
+++ encrypt.py
@@ -18,7 +18,7 @@ cipher = Fernet(key)
 # the format of key-value pairs. In this case, it includes information such as a username, password,
 # site URL, and relative URL. This data is going to be encrypted using the Fernet encryption algorithm
 # before being saved to a file for secure storage or transmission.
-config_data = f"USERNAME=jim.nakamura@cabotcorp.com\nPASSWORD=Otaku2010.....\ndatasource=maua-ntp01\nimstype=aspenone\nurl=http://maua-ntp01/ProcessData/AtProcessDataREST.dll\ndatabase=report_db\nSQLusername=ntp01read\nSQLpassword=%Cabot19\nsite_url=https://cabotcorp.sharepoint.com/sites/MauaWPS/".encode("utf-8")
+config_data = f"USERNAME=jim.nakamura@cabotcorp.com\nPASSWORD=Otaku2010......\ndatasource=maua-ntp01\nimstype=aspenone\nurl=http://maua-ntp01/ProcessData/AtProcessDataREST.dll\ndatabase=report_db\nSQLusername=ntp01read\nSQLpassword=%Cabot19\nsite_url=https://cabotcorp.sharepoint.com/sites/MauaWPS/".encode("utf-8")
 
 # Encriptação dos dados
 encrypted_data = cipher.encrypt(config_data)
